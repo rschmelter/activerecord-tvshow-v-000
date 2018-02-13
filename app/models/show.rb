24a085c7036dd34b9dc179376c46d2efc78684b1
Ryan Schmelter <rschmelter33@gmail.com>
2018-02-13 21:44:55 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    show = self.where("rating = highest_rating")
+    show = self.where("rating = #{highest_rating}")
     show.name
   end
 
